1326196b8c828a0d603f5b270fac7cc0552b9ef8
Antonin-Seichepine <antonin.seichepine--berradj@edu.univ-fcomte.fr>
2026-01-30 09:26:29 +0100

    FInal

--- src/backend.mjs
+++ src/backend.mjs
@@ -1,23 +1,19 @@
 import PocketBase from "pocketbase";
-
-const db = new PocketBase("http://127.0.0.1:8090");
+const pb = new PocketBase("http://127.0.0.1:8090");
 
 export function getImageUrl(record, imageField) {
-    if (!record || !record[imageField]) return null;
+    const file = Array.isArray(record[imageField])
+        ? record[imageField][0]
+        : record[imageField];
 
-    const value = record[imageField];
-    const filename = Array.isArray(value) ? value[0] : value; // 1ère image si multiple
-
-    if (!filename) return null;
-    return db.files.getUrl(record, filename);
+    return file ? pb.files.getUrl(record, file) : null;
 }
 
 export async function getOffres() {
     try {
-        const data = await db.collection("maison").getFullList({
+        return await pb.collection("maison").getFullList({
             sort: "-created",
         });
-        return data;
     } catch (error) {
         console.log("Une erreur est survenue en lisant la liste des maisons", error);
         return [];
